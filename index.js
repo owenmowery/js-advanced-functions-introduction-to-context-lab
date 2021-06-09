5d92228c046dc73bf7b3f4d1dc9cce987c20f9a6
Owen Mowery <owenmowery99@gmail.com>
2021-06-08 23:00:16 -0400

    commit tree eaf66152d78bcdb18cba20505f51da89f8440ecf

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code here
+function createEmployeeRecord(arr) {
+    return {firstName: arr[0], familyName: arr[1], title: arr[2], payPerHour: arr[3], timeInEvents: [], timeOutEvents: []};
+};
+
+function createEmployeeRecords(arr) {
+    byebug
+};
